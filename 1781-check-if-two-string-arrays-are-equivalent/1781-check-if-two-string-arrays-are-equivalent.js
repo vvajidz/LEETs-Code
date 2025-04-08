8a5d4eccdc63416e10da822190639d2572045357
vvajidz <vvajidz56@gmail.com>
2025-04-08 14:37:18 +0530

    Time: 0 ms (100%), Space: 52.7 MB (76.78%) - LeetHub

---
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let arr1 = word1.join("")
+    let arr2 = word2.join("")
+
+    return arr1==arr2
+
+    
+};
