20c9cdcd17944f9f17b5d29553c3f32722819433
vvajidz <vvajidz56@gmail.com>
2025-06-19 12:18:36 +0530

    Time: 1 ms (14.5%), Space: 55.6 MB (6.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,15 +5,4 @@
  */
 var searchInsert = function(nums, target) {
 
- for (let i = 0 ; i < nums.length ; i++){
-    if(nums[i] == target){
-        return i
-    }
-    if(nums[i] > target){
-       
-        return i
-    } 
- }
-
- return nums.length 
-};
+ for(let i = 0 ; i < nums.length ; i++){if(nums[i] == target){return i}if(nums[i] > target){return i}}return nums.length};
